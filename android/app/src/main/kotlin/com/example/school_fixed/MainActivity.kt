640d810cec310e24d89af6acdf677fac8cf0284f
Alexey121232 <alexeyofficalrddm@gmail.com>
2025-05-18 14:12:09 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/school_fixed/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/school_fixed/MainActivity.kt
@@ -3,4 +3,5 @@ package com.example.school_for_teach
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    // Ничего не нужно добавлять — главное, чтобы не было старых override'ов.
 }
